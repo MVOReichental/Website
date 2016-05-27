2f7844bbf854c8d687d91ef6c5c90715ead60133
Michael Wieland <git@selfcoders.com>
2016-05-27 22:04:08 +0200

    Catch all exceptions and show error page on error Set HTTP status code to 404 on Not Found

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,39 +1,47 @@
 <?php
 use de\mvo\Database;
-use de\mvo\renderer\StaticRenderer;
+use de\mvo\renderer\NotFoundRenderer;
 use de\mvo\renderer\utils\MustacheRenderer;
 use de\mvo\router\Endpoints;
 use de\mvo\router\Router;
 
-require_once __DIR__ . "/../bootstrap.php";
-
-Database::init();
-
-$router = new Router;
-
-$router->mapAll(Endpoints::get());
-
-$match = $router->match($_SERVER["PATH_INFO"]);
-if ($match === null)
+try
 {
-	$renderer = new StaticRenderer("not-found");
+	require_once __DIR__ . "/../bootstrap.php";
+
+	Database::init();
+
+	$router = new Router;
+
+	$router->mapAll(Endpoints::get());
+
+	$match = $router->match($_SERVER["PATH_INFO"]);
+	if ($match === null)
+	{
+		$renderer = new NotFoundRenderer;
+	}
+	else
+	{
+		$renderer = $match->renderer;
+	}
+
+	$view = $renderer->render();
+	if ($view === null)
+	{
+		echo $view;
+	}
+	else
+	{
+		echo MustacheRenderer::render("main", array
+		(
+			"view" => $view,
+			"currentYear" => date("Y")
+		));
+	}
 }
-else
+catch (Exception $exception)
 {
-	$renderer = $match->renderer;
-}
-
-$view = $renderer->render();
-
-if (headers_sent())
-{
-	echo $view;
-}
-else
-{
-	echo MustacheRenderer::render("main", array
-	(
-		"view" => $view,
-		"currentYear" => date("Y")
-	));
+	http_response_code(500);
+	readfile(__DIR__ . "/error-500.html");
+	error_log($exception);
 }
