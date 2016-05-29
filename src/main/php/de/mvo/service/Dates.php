6c2103ebd03ecf1bb34539e51d72c969d75123dd
Michael Wieland <git@selfcoders.com>
2016-05-29 17:09:01 +0200

    Create iCalendar feed from dates

--- src/main/php/de/mvo/service/Dates.php
+++ src/main/php/de/mvo/service/Dates.php
@@ -2,7 +2,10 @@
 namespace de\mvo\service;
 
 use de\mvo\model\date\DateList;
+use de\mvo\model\date\Entry;
 use de\mvo\MustacheRenderer;
+use Eluceo\iCal\Component\Calendar;
+use Eluceo\iCal\Component\Event;
 
 class Dates extends AbstractService
 {
@@ -17,7 +20,29 @@ class Dates extends AbstractService
 
 	public function getIcal()
 	{
-		// TODO: Build iCal
+		$calendar = new Calendar($_SERVER["HTTP_HOST"]);
+
+		$dates = new DateList;
+
+		/**
+		 * @var $date Entry
+		 */
+		foreach ($dates as $date)
+		{
+			$event = new Event;
+
+			$event->setDtStart($date->startDate);
+			$event->setDtEnd($date->endDate);
+
+			$event->setNoTime(!$date->startDate->hasTime());
+
+			$event->setSummary($date->title);
+
+			$calendar->addComponent($event);
+		}
+
+		header("Content-Type: text/calendar; charset=utf-8");
+		echo $calendar->render();
 		return null;
 	}
 }
