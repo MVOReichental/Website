effd886272b66cfa53ef8c67a684cd15f2dde430
Michael Wieland <git@selfcoders.com>
2016-05-29 18:24:53 +0200

    Allow to include additional HTML content on news page

--- src/main/php/de/mvo/service/News.php
+++ src/main/php/de/mvo/service/News.php
@@ -9,8 +9,19 @@ class News extends AbstractService
 {
 	public function get()
 	{
+		$newsFile = RESOURCES_ROOT . "/news.html";
+		if (file_exists($newsFile))
+		{
+			$newsContent = file_get_contents($newsFile);
+		}
+		else
+		{
+			$newsContent = null;
+		}
+
 		return MustacheRenderer::render("news", array
 		(
+			"news" => $newsContent,
 			"dates" => new DateList(3),
 			"albums" => AlbumList::getLatest(3)
 		));
