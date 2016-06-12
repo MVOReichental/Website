7ccf6acfe34a392d8dc597f278f5e2b587b0079e
Michael Wieland <git@selfcoders.com>
2016-06-12 17:50:02 +0200

    Added script for listing permissions of a user

---
+++ bin/get-permissions-of-user.php
@@ -0,0 +1,30 @@
+<?php
+use de\mvo\Database;
+use de\mvo\model\permissions\GroupList;
+use de\mvo\model\users\User;
+
+require_once __DIR__ . "/../bootstrap.php";
+
+if (!isset($argv[1]))
+{
+	echo "Usage: " . $argv[0] . " <username>\n";
+	exit(1);
+}
+
+$username = $argv[1];
+
+Database::init();
+
+$user = User::getByUsername($username);
+if ($user === null)
+{
+	echo "User '" . $username . "' not found!\n";
+	exit(1);
+}
+
+$permissions = GroupList::load()->getPermissionsForUser($user);
+
+foreach ($permissions as $permission)
+{
+	echo $permission . "\n";
+}
