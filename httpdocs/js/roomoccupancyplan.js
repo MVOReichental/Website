452a8994aca76b16f78d37d87e62d7f6d7d478ef
Michael Wieland <git@selfcoders.com>
2016-11-07 21:12:39 +0100

    Enable calendar edit mode only if user has permission

--- httpdocs/js/roomoccupancyplan.js
+++ httpdocs/js/roomoccupancyplan.js
@@ -1,9 +1,11 @@
 $(function () {
-    $("#roomoccupancyplan-calendar").fullCalendar({
+    var container = $("#roomoccupancyplan-calendar");
+
+    container.fullCalendar({
         locale: "de",
         defaultView: "agendaWeek",
         events: "internal/roomoccupancyplan/entries.json",
-        editable: true,
+        editable: container.data("editable"),
         eventDrop: function (event) {
             $.post("internal/roomoccupancyplan/entries/" + event.id, {
                 start: event.start.format(),
