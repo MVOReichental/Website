d33306349035bd6c5835bee77765ebb3747b732f
Michael Wieland <git@selfcoders.com>
2016-11-16 00:27:30 +0100

    image2 plugin is not used (image should not be resized to make sure it auto fits to all devices)

--- httpdocs/js/newseditor.js
+++ httpdocs/js/newseditor.js
@@ -1,7 +1,7 @@
 $(function () {
     $("#news-content").ckeditor({
         language: "de",
-        extraPlugins: "colorbutton,image2,inlinesave,justify,uploadimage",
+        extraPlugins: "colorbutton,inlinesave,justify,uploadimage",
         uploadUrl: "/internal/upload",
         inlinesave: {
             postUrl: "/internal/admin/newseditor"
