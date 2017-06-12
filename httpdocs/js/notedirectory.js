b2682018ad6d75b2a6179e2f7f4be79fd5459819
Michael Wieland <git@selfcoders.com>
2017-06-12 20:57:21 +0200

    Do not show column selection if notedirectory list is not available on current page

--- httpdocs/js/notedirectory.js
+++ httpdocs/js/notedirectory.js
@@ -1,35 +1,41 @@
 $(function () {
     var columnSelection = $("#notedirectory-columns");
 
-    $("#notedirectory-list").find("th").each(function () {
-        var name = $(this).data("name");
+    var list = $("#notedirectory-list");
 
-        if (!name) {
-            return;
-        }
-
-        var optionElement = $("<option>");
-
-        optionElement.attr("value", name);
-        optionElement.text($(this).text());
-        optionElement.prop("selected", true);
-
-        columnSelection.append(optionElement);
-    });
-
-    columnSelection.selectpicker();
-
-    columnSelection.on("changed.bs.select", function () {
-        var columns = $(this).val();
-
-        $("#notedirectory-list").find("th, td").each(function () {
+    if (list.length) {
+        list.find("th").each(function () {
             var name = $(this).data("name");
 
             if (!name) {
                 return;
             }
 
-            $(this).toggle(columns.indexOf(name) !== -1);
+            var optionElement = $("<option>");
+
+            optionElement.attr("value", name);
+            optionElement.text($(this).text());
+            optionElement.prop("selected", true);
+
+            columnSelection.append(optionElement);
         });
-    });
+
+        columnSelection.selectpicker();
+
+        columnSelection.on("changed.bs.select", function () {
+            var columns = $(this).val();
+
+            $("#notedirectory-list").find("th, td").each(function () {
+                var name = $(this).data("name");
+
+                if (!name) {
+                    return;
+                }
+
+                $(this).toggle(columns.indexOf(name) !== -1);
+            });
+        });
+    } else {
+        columnSelection.hide();
+    }
 });
