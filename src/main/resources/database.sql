04260f8a50abdfa619b5036b9f954243d600df6f
Michael Wieland <git@selfcoders.com>
2017-07-19 18:24:15 +0200

    Format database schema

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,205 +1,268 @@
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
 
 CREATE TABLE `locations` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
+  `id`        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`      VARCHAR(200)     NOT NULL,
+  `latitude`  FLOAT                     DEFAULT NULL,
+  `longitude` FLOAT                     DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `dates` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `startDate` datetime NOT NULL,
-  `endDate` datetime DEFAULT NULL,
-  `title` varchar(200) NOT NULL,
-  `description` text DEFAULT NULL,
-  `locationId` int(11) unsigned DEFAULT NULL,
-  `highlight` bool NOT NULL DEFAULT FALSE,
-  `isPublic` bool NOT NULL DEFAULT TRUE,
+  `id`          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `startDate`   DATETIME         NOT NULL,
+  `endDate`     DATETIME                  DEFAULT NULL,
+  `title`       VARCHAR(200)     NOT NULL,
+  `description` TEXT                      DEFAULT NULL,
+  `locationId`  INT(11) UNSIGNED          DEFAULT NULL,
+  `highlight`   BOOL             NOT NULL DEFAULT FALSE,
+  `isPublic`    BOOL             NOT NULL DEFAULT TRUE,
   PRIMARY KEY (`id`),
   KEY (`locationId`),
-  CONSTRAINT FOREIGN KEY (`locationId`) REFERENCES `locations` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`locationId`) REFERENCES `locations` (`id`)
+    ON UPDATE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `dategroups` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `dateId` int(11) unsigned NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `id`     INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `dateId` INT(11) UNSIGNED NOT NULL,
+  `name`   VARCHAR(100)     NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`dateId`, `name`),
-  CONSTRAINT FOREIGN KEY (`dateId`) REFERENCES `dates` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`dateId`) REFERENCES `dates` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `newEmail` varchar(100) DEFAULT NULL,
-  `newEmailKey` varchar(32) DEFAULT NULL,
-  `newEmailDate` datetime DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `resetPasswordKey` varchar(32) DEFAULT NULL,
-  `resetPasswordDate` datetime DEFAULT NULL,
-  `requirePasswordChange` boolean NOT NULL DEFAULT FALSE,
-  `firstName` varchar(100) DEFAULT NULL,
-  `lastName` varchar(100) DEFAULT NULL,
-  `birthDate` date DEFAULT NULL,
-  `totpKey` varchar(20) DEFAULT NULL,
-  `lastOnline` datetime DEFAULT NULL,
-  `enabled` boolean NOT NULL DEFAULT TRUE,
+  `id`                    INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username`              VARCHAR(100)     NOT NULL,
+  `email`                 VARCHAR(100)              DEFAULT NULL,
+  `newEmail`              VARCHAR(100)              DEFAULT NULL,
+  `newEmailKey`           VARCHAR(32)               DEFAULT NULL,
+  `newEmailDate`          DATETIME                  DEFAULT NULL,
+  `password`              VARCHAR(255)              DEFAULT NULL,
+  `resetPasswordKey`      VARCHAR(32)               DEFAULT NULL,
+  `resetPasswordDate`     DATETIME                  DEFAULT NULL,
+  `requirePasswordChange` BOOLEAN          NOT NULL DEFAULT FALSE,
+  `firstName`             VARCHAR(100)              DEFAULT NULL,
+  `lastName`              VARCHAR(100)              DEFAULT NULL,
+  `birthDate`             DATE                      DEFAULT NULL,
+  `totpKey`               VARCHAR(20)               DEFAULT NULL,
+  `lastOnline`            DATETIME                  DEFAULT NULL,
+  `enabled`               BOOLEAN          NOT NULL DEFAULT TRUE,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `usedtotptokens` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `userId` int(11) unsigned NOT NULL,
-  `token` varchar(6) NOT NULL,
-  `date` datetime NOT NULL,
+  `id`     INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) UNSIGNED NOT NULL,
+  `token`  VARCHAR(6)       NOT NULL,
+  `date`   DATETIME         NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`userId`, `token`),
-  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `usercontacts` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `userId` int(11) unsigned NOT NULL,
-  `type` set('phone', 'mobile') NOT NULL,
-  `category` set('private', 'business') NOT NULL,
-  `value` varchar(200) NOT NULL,
+  `id`       INT(11) UNSIGNED            NOT NULL AUTO_INCREMENT,
+  `userId`   INT(11) UNSIGNED            NOT NULL,
+  `type`     SET ('phone', 'mobile')     NOT NULL,
+  `category` SET ('private', 'business') NOT NULL,
+  `value`    VARCHAR(200)                NOT NULL,
   PRIMARY KEY (`id`),
   KEY (`userId`),
-  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `uploads` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `key` varchar(8) NOT NULL,
-  `filename` varchar(200) NOT NULL,
+  `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `key`      VARCHAR(8)       NOT NULL,
+  `filename` VARCHAR(200)     NOT NULL,
   PRIMARY KEY (`id`),
   KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `messages` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL,
-  `senderUserId` int(11) unsigned NOT NULL,
-  `text` longtext NOT NULL,
-  `visibleToSender` boolean DEFAULT TRUE,
+  `id`              INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date`            DATETIME         NOT NULL,
+  `senderUserId`    INT(11) UNSIGNED NOT NULL,
+  `text`            LONGTEXT         NOT NULL,
+  `visibleToSender` BOOLEAN                   DEFAULT TRUE,
   PRIMARY KEY (`id`),
   KEY (`senderUserId`),
-  CONSTRAINT FOREIGN KEY (`senderUserId`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`senderUserId`) REFERENCES `users` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `messagerecipients` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `messageId` int(11) unsigned NOT NULL,
-  `userId` int(11) unsigned NOT NULL,
-  `visible` boolean DEFAULT TRUE,
+  `id`        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `messageId` INT(11) UNSIGNED NOT NULL,
+  `userId`    INT(11) UNSIGNED NOT NULL,
+  `visible`   BOOLEAN                   DEFAULT TRUE,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`messageId`, `userId`),
-  CONSTRAINT FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `messageattachments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `messageId` int(11) unsigned NOT NULL,
-  `uploadId` int(11) unsigned NOT NULL,
+  `id`        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `messageId` INT(11) UNSIGNED NOT NULL,
+  `uploadId`  INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`messageId`, `uploadId`),
-  CONSTRAINT FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT FOREIGN KEY (`uploadId`) REFERENCES `uploads` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`messageId`) REFERENCES `messages` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FOREIGN KEY (`uploadId`) REFERENCES `uploads` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `forms` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `filename` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `title` varchar(200) NOT NULL,
+  `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `filename` VARCHAR(100)     NOT NULL,
+  `name`     VARCHAR(100)     NOT NULL,
+  `title`    VARCHAR(200)     NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `notedirectoryprograms` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `year` year NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `title` varchar(200) NOT NULL,
+  `id`    INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `year`  YEAR             NOT NULL,
+  `name`  VARCHAR(200)     NOT NULL,
+  `title` VARCHAR(200)     NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`year`, `name`)
-) Engine=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `notedirectorytitles` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `composer` varchar(200) DEFAULT NULL,
-  `arranger` varchar(200) DEFAULT NULL,
-  `publisher` varchar(200) DEFAULT NULL,
+  `id`        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`     VARCHAR(200)     NOT NULL,
+  `composer`  VARCHAR(200)              DEFAULT NULL,
+  `arranger`  VARCHAR(200)              DEFAULT NULL,
+  `publisher` VARCHAR(200)              DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `notedirectoryprogramtitles` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `programId` int(11) unsigned NOT NULL,
-  `titleId` int(11) unsigned NOT NULL,
-  `number` int(11) unsigned NOT NULL,
+  `id`        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `programId` INT(11) UNSIGNED NOT NULL,
+  `titleId`   INT(11) UNSIGNED NOT NULL,
+  `number`    INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`programId`, `titleId`),
-  CONSTRAINT FOREIGN KEY (`programId`) REFERENCES `notedirectoryprograms` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT FOREIGN KEY (`titleId`) REFERENCES `notedirectorytitles` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`programId`) REFERENCES `notedirectoryprograms` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FOREIGN KEY (`titleId`) REFERENCES `notedirectorytitles` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `protocols` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `uploadId` int(11) unsigned NOT NULL,
-  `title` varchar(200) NOT NULL,
-  `date` date NOT NULL,
+  `id`       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uploadId` INT(11) UNSIGNED NOT NULL,
+  `title`    VARCHAR(200)     NOT NULL,
+  `date`     DATE             NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`uploadId`),
-  CONSTRAINT FOREIGN KEY (`uploadId`) REFERENCES `uploads` (`id`) ON UPDATE CASCADE ON DELETE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`uploadId`) REFERENCES `uploads` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE `protocolgroups` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `protocolId` int(11) unsigned NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `protocolId` INT(11) UNSIGNED NOT NULL,
+  `name`       VARCHAR(100)     NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`protocolId`, `name`),
-  CONSTRAINT FOREIGN KEY (`protocolId`) REFERENCES `protocols` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`protocolId`) REFERENCES `protocols` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `roomoccupancyplan` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `startTime` time NOT NULL,
-  `endTime` time NOT NULL,
-  `date` date NOT NULL,
-  `repeatTillDate` date DEFAULT NULL,
-  `repeatWeekly` boolean NOT NULL DEFAULT TRUE,
-  `title` varchar(200) NOT NULL,
+  `id`             INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `startTime`      TIME             NOT NULL,
+  `endTime`        TIME             NOT NULL,
+  `date`           DATE             NOT NULL,
+  `repeatTillDate` DATE                      DEFAULT NULL,
+  `repeatWeekly`   BOOLEAN          NOT NULL DEFAULT TRUE,
+  `title`          VARCHAR(200)     NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE `visits` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `ip` varchar(50) NOT NULL,
-  `date` date NOT NULL,
-  `firstVisit` time NOT NULL,
-  `lastVisit` time NOT NULL,
-  `userId` int(11) unsigned DEFAULT NULL,
+  `id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ip`         VARCHAR(50)      NOT NULL,
+  `date`       DATE             NOT NULL,
+  `firstVisit` TIME             NOT NULL,
+  `lastVisit`  TIME             NOT NULL,
+  `userId`     INT(11) UNSIGNED          DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`ip`, `date`, `userId`),
-  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
