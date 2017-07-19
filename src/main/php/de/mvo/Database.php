b3b590b24c06d130a7b056b9a7f5093d7a61030b
Michael Wieland <git@selfcoders.com>
2017-07-19 18:05:54 +0200

    Charset should be set in DSN using "charset" option

--- src/main/php/de/mvo/Database.php
+++ src/main/php/de/mvo/Database.php
@@ -16,7 +16,6 @@ class Database
 
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_CLASS);
-        self::$pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
     }
 
     public static function prepare($statement)
