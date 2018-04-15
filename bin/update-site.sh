266b92b86bb76c9bbe0d9c37303647947fa6106a
Michael Wieland <git@selfcoders.com>
2018-04-15 17:15:27 +0200

    Add script to update site

---
+++ bin/update-site.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+cd $(dirname $0)/..
+
+git pull
+composer install --no-dev
+rm -rf src/main/resources/twig-cache
+pushd httpdocs && (npm install; popd)
+git rev-parse HEAD > version
