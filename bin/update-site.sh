61a7b7b2d4678e79cece8202c2977bbfc129537d
Michael Wieland <git@selfcoders.com>
2018-04-15 17:18:49 +0200

    Explicit check for correct execution of pushd is not required (set -e)

--- bin/update-site.sh
+++ bin/update-site.sh
@@ -7,5 +7,7 @@ cd $(dirname $0)/..
 git pull
 composer install --no-dev
 rm -rf src/main/resources/twig-cache
-pushd httpdocs && (npm install; popd)
+pushd httpdocs
+npm install
+popd
 git rev-parse HEAD > version
