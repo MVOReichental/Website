329e49e921abcc67549b8ee763645aac0ed147de
Michael Wieland <git@selfcoders.com>
2019-08-01 21:05:00 +0200

    Add missing parent constructor call

--- src/main/php/de/mvo/model/GroupMembersList.php
+++ src/main/php/de/mvo/model/GroupMembersList.php
@@ -9,6 +9,8 @@ class GroupMembersList extends ArrayObject
 {
     public function __construct($group)
     {
+        parent::__construct();
+
         $rootGroup = GroupList::load()->getGroupByPermission("group." . $group);
         if ($rootGroup === null) {
             return;
