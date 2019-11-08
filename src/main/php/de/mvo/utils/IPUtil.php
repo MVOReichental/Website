91e023c9a4cb0072afefe07b2e6bc59dcc53c8c1
Michael Wieland <git@selfcoders.com>
2019-11-08 21:39:21 +0100

    Allow to configure trusted proxies in IP/CIDR format

--- src/main/php/de/mvo/utils/IPUtil.php
+++ src/main/php/de/mvo/utils/IPUtil.php
@@ -5,11 +5,32 @@ class IPUtil
 {
     public static function getClientIP($trustedProxies = [])
     {
-        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) and in_array($_SERVER["REMOTE_ADDR"], $trustedProxies)) {
-            $ips = explode(",", $_SERVER["HTTP_X_FORWARDED_FOR"]);
-            return trim(end($ips));
+        if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            foreach ($trustedProxies as $trustedProxy) {
+                if (self::isIPInRange($_SERVER["REMOTE_ADDR"], $trustedProxy)) {
+                    $ips = explode(",", $_SERVER["HTTP_X_FORWARDED_FOR"]);
+                    return trim(end($ips));
+                }
+            }
         }
 
         return $_SERVER["REMOTE_ADDR"];
+    }
+
+    // Taken from https://gist.github.com/tott/7684443
+    public static function isIPInRange($ip, $range)
+    {
+        if (strpos($range, "/") === false) {
+            $range .= "/32";
+        }
+
+        // $range is in IP/CIDR format eg 127.0.0.1/24
+        list($range, $netmask) = explode("/", $range, 2);
+        $rangeDecimal = ip2long($range);
+        $ipDecimal = ip2long($ip);
+        $wildcardDecimal = pow(2, (32 - $netmask)) - 1;
+        $netmaskDecimal = ~$wildcardDecimal;
+
+        return (($ipDecimal & $netmaskDecimal) == ($rangeDecimal & $netmaskDecimal));
     }
 }
